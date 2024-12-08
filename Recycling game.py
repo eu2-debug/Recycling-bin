2e0f9bab142ecbe117e5007921bfdafff9e0cf39
eu2-debug <emileuju@gmail.com>
2024-12-08 17:56:32 +0000

    Update Recycling game.py

--- Recycling game.py
+++ Recycling game.py
@@ -36,15 +36,15 @@ def draw():
     screen.clear()
     screen.blit("bground",(0,0))
     if game_over == True:
-        display_message("GAME OVER")
+        display_message("GAME OVER","You lose")
     elif game_complete == True:
-        display_message("You are a recycler!")
+        display_message("You are a recycler!","You win")
     else:
         for item in items:
             item.draw()
-            
+
 def update():
-    global items 
+    global items
     if len(items) == 0:
         items = make_items(current_level)
 
@@ -54,14 +54,14 @@ def make_items(number_of_extra_items):
     layout_items(new_items)
     animate_items(new_items)
     return new_items
-    
+
 def get_option_to_create(number_of_extra_items):
     items_to_create = ["paper"]
     for i in range(0,number_of_extra_items):
         random_option = random.choice(ITEMS)
         items_to_create.append(random_option)
     return items_to_create
-     
+
      #creating items for the game
 def create_items(items_to_create):
     new_items = []
@@ -69,10 +69,71 @@ def create_items(items_to_create):
         item = Actor(option+"img")
         new_items. append(item)
     return new_items
-    
-    
-        
- 
+
+def layout_items(items_to_layout):
+    number_of_gap = len(items_to_layout)+1
+    gap_size = WIDTH/number_of_gap
+    random.shuffle(items_to_layout)
+    for index, item in enumerate(items_to_layout):
+        new_x_pos = (index+1)*gap_size
+        item.x = new_x_pos
+
+def animate_items(items_to_animate):
+    global animations
+    for item in items_to_animate:
+        duration = START_SPEED - current_level
+        item.anchor = ("center","bottom")
+        animation = animate(item, duration = duration, on_finished = handle_game_over, y = HEIGHT)
+        animations.append(animation)
+
+def handle_game_over():
+    global game_over
+    game_over = True
+
+def on_mouse_down(pos):
+    global items, current_level
+    for item in items:
+        if item.collidepoint(pos):
+            if "paper" in item.image:
+                handle_game_complete()
+            else:
+                handle_game_over()
+
+def handle_game_complete():
+    global current_level, items, animations, game_complete
+    stop_animations(animations)
+    if current_level == FINAL_LEVEL:
+        game_complete = True
+    else:
+        current_level += 1
+        items = []
+        animations = []
+
+def stop_animations(animations_to_stop):
+    for animation in animations_to_stop:
+        if animation.running:
+            animation.stop()
+
+def display_message(heading_text, sub_heading_text):
+    screen.draw.text(heading_text, fontsize=60, center=CENTRE, color="black")
+    screen.draw.text(sub_heading_text, fontsize=30, center=(CENTRE_X, CENTRE_Y + 30), color="black")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
